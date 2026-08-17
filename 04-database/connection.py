057469a139f7ac1d426b0c03d4a47d46f9744e48
unknown <kodanemanthan@gmail.com>
2026-08-17 19:14:47 +0530

    Add backend and database directories

---
+++ 04-database/connection.py
@@ -0,0 +1,15 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGODB_URL = os.getenv("MONGODB_URL")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "drishti")
+
+client = MongoClient(MONGODB_URL)
+
+db = client[DATABASE_NAME]
+
+client.admin.command("ping")
+print("MongoDB connected successfully!")
